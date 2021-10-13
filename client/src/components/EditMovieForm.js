3e352d98e55e9a8ca2b20a0a83cd9679f787d87b
Kat <kyevsukov@gmail.com>
2021-10-13 18:14:19 -0400

    can edit movie

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -22,7 +22,10 @@ const EditMovieForm = (props) => {
 		.then(res => {
 			setMovie(res.data)
 		})
-	})
+		.catch(err => {
+			console.log(err)
+		})
+	}, [])
 	
 	const handleChange = (e) => {
         setMovie({
@@ -33,6 +36,14 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
+		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+		.then(res => {
+			props.setMovies(res.data)
+			push(`/movies/${id}`)
+		})
+		.catch(err => {
+			console.log(err)
+		})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
